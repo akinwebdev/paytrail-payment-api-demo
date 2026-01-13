b21e367ec348b6b63b18ed19c8442cf3c4047b3c
akinwebdev <akintoksan+webdev@gmail.com>
2026-01-13 01:40:20 +0000

    Update Klarna API endpoint to remove partner account ID from path
    - Change from /v2/accounts/{partner_account_id}/payment/requests
    - To: /v2/accounts/payment/requests
    - Update both POST and GET endpoints

--- server.js
+++ server.js
@@ -556,7 +556,7 @@ app.post('/api/klarna/payment-request', async (req, res) => {
         // For Klarna API, the API key is used as the username with empty password
         const auth = Buffer.from(`${KLARNA_API_KEY}:`).toString('base64');
 
-        endpointUrl = `${KLARNA_API_URL}/v2/accounts/${KLARNA_PARTNER_ACCOUNT_ID}/payment/requests`;
+        endpointUrl = `${KLARNA_API_URL}/v2/accounts/payment/requests`;
         console.log('📤 Klarna API endpoint:', endpointUrl);
         console.log('📤 Partner Account ID:', KLARNA_PARTNER_ACCOUNT_ID);
         console.log('📤 API URL:', KLARNA_API_URL);
